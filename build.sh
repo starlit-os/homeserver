20b588d545d20786cd57bb14ef1dc69339eb2204
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-01-11 10:17:11 +0100

    feat: Add incus ui

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ ln -sr /etc/containers/systemd/*.container /usr/lib/bootc/bound-images.d/
 dnf install -y cockpit cockpit-machines cockpit-podman cockpit-files
 
 # Incus UI
-# curl -Lo /tmp/incus-ui-canonical.deb \
-#     https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/"$(curl https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/ | grep -E incus-ui-canonical | cut -d '"' -f 2 | sort -r | head -1)"
+curl -Lo /tmp/incus-ui-canonical.deb \
+     https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/"$(curl https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/ | grep -E incus-ui-canonical | cut -d '"' -f 2 | sort -r | head -1)"
 
-# ar -x --output=/tmp /tmp/incus-ui-canonical.deb
-# tar --zstd -xvf /tmp/data.tar.zst
-# mv /opt/incus /usr/lib/
-# sed -i 's@\[Service\]@\[Service\]\nEnvironment=INCUS_UI=/usr/lib/incus/ui/@g' /usr/lib/systemd/system/incus.service
+ar -x --output=/tmp /tmp/incus-ui-canonical.deb
+tar --zstd -xvf /tmp/data.tar.zst
+mv /opt/incus /usr/lib/
+sed -i 's@\[Service\]@\[Service\]\nEnvironment=INCUS_UI=/usr/lib/incus/ui/@g' /usr/lib/systemd/system/incus.service
 
 # Services
 
