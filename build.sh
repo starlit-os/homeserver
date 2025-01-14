4347edec20b6be65115b6b7c5104e9a3313742e6
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-01-14 20:19:55 +0100

    feat: Add cockpit-selinux

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # Packages
 
-dnf install -y cockpit cockpit-machines cockpit-podman cockpit-files unzip
+dnf install -y cockpit cockpit-machines cockpit-podman cockpit-files cockpit-selinux unzip
 
 # Docker install: https://docs.docker.com/engine/install/centos/#install-using-the-repository
 dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
@@ -14,7 +14,7 @@ dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker
 curl -L $(curl -s https://api.github.com/repos/azukaar/Cosmos-Server/releases/latest | \
     sed 's/[()",{}]/ /g; s/ /\n/g' | grep "https.*releases/download.*amd64.zip$") \
     -o /tmp/cosmos-server.zip
-mkdir /usr/lib/cosmos-cloud
+mkdir /var/lib/cosmos-cloud
 unzip /tmp/cosmos-server.zip -d /tmp
 mv /tmp/cosmos-cloud-*/* /var/lib/cosmos-cloud
 
